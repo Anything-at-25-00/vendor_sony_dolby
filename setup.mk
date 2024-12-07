65ef525fd976ad710c03e270871960412ccc521e
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2024-12-07 15:34:49 +0530

    Dolby: Import blobs from `https://github.com/reiryuki/Dolby-Atmos-Sony-Xperia-5-V-Magisk-Module.git`
    * dax-default.xml unmodified directly picked from https://dumps.tadiphone.dev/dumps/sony/pdx237
    
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

---
+++ setup.mk
@@ -0,0 +1,19 @@
+# Inherit dolby configs
+$(call inherit-product, vendor/sony/dolby/dolby-vendor.mk)
+
+# SEPolicy
+BOARD_VENDOR_SEPOLICY_DIRS += vendor/sony/dolby/sepolicy/vendor
+
+# Dolby properties
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.vendor.audio_fx.current=dolby \
+    ro.vendor.audio.dolby.dax.support=true \
+    ro.vendor.dolby.dax.version=DAX3_3.7.0.8_r1 \
+    ro.audio.monitorrotation=true \
+    ro.audio.monitorWindowRotation=true \
+    vendor.audio.dolby.ds2.enabled=false \
+    vendor.audio.dolby.ds2.hardbypass=false
+
+# Audio configs
+AUDIO_FEATURE_ENABLED_DS2_DOLBY_DAP := true
+TARGET_EXCLUDES_AUDIOFX := true
